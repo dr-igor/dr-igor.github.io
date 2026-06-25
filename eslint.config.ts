cd700d0fb7aecc776ae86eb6a2a9dfb7b633f73d
Igor Petrik <dr@igorpetrik.com>
2026-06-24 20:38:38 -0700

    chore: Adds common linting rules

---
+++ eslint.config.ts
@@ -0,0 +1,25 @@
+import base from '@igor/eslint-config-typescript'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'postcss.config.js', 'tailwind.config.js', 'eslint.config.ts'] },
+  ...base,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+  },
+)
